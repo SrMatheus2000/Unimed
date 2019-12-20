c544cb203778da6148926d12ab70c1998db87538
unknown <ti@UVCTI004.univc.local>
2019-12-20 16:58:55 -0300

    Primos

--- src/Primos.js
+++ src/Primos.js
@@ -1,10 +1,55 @@
 import React, { Component } from "react";
 
-export class Primos extends Component{
+const primos = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113];
 
-    mostrarPrimos(){
-        <ul>        
-            {      
+const looped = primos.map((data,i) =>(
+    <ul>
+        <li>{data}</li>
+    </ul>
+));
+
+export class Primos extends Component{
+    
+    state={
+        mostrarPrimos:false
+    }
+
+    mostrarPrimos = () => {
+        this.setState(prevState => {
+          console.log(prevState);
+          return { isClicked: !prevState.isClicked };
+        });
+      };
+
+
+    // mostrarPrimos=()=>{
+        
+    //     return(
+    //         <ul>        
+    //             {      
+    //                 primos.map((val, index) => {
+    //                     return (
+    //                         <li key={index}>
+    //                         { val }
+    //                         </li>
+    //                     );
+    //                 })
+    //             }
+    //         </ul>
+    //     );
+    // }
+
+    render(){
+        return (
+            // React.createElement("button", {onClick: this.mostrarPrimos}, "Mostrar Primos")
+            
+            // <React.Fragment>
+            <div>
+               <button onClick={this.mostrarPrimos}>Mostrar Primos</button>
+               {this.state.isClicked && looped}
+            </div>
+            /* <ul>       
+            {    
                 primos.map((val, index) => {
                     return (
                         <li key={index}>
@@ -13,18 +58,8 @@ export class Primos extends Component{
                     );
                 })
             }
-        </ul>
-    }
-
-    render(){
-        let primos = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113];
-        return (
-            // React.createElement("button", {onClick: this.mostrarPrimos}, "Mostrar Primos"),
-            
-            <React.Fragment>
-                <button onClick={this.mostrarPrimos}>Mostrar Primos</button>
-                
-            </React.Fragment>
+        </ul> */
+            /* </React.Fragment> */
         );
     }
 }
